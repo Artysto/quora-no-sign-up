f134595fd7b4ab9f01ce3b89f29f82eaea07ca8c
Artysto <pandeyshailendra102@gmail.com>
2020-05-07 22:19:11 +0530

    Removing one redundant line

--- content.js
+++ content.js
@@ -1,5 +1,3 @@
-id_name = document.querySelector('[id$="signup_wall_wrapper"]').id;
-div_pop_up = document.getElementById(id_name);
+div_pop_up = document.querySelector('[id$="signup_wall_wrapper"]');
 div_pop_up.style.display = "none";
 document.body.removeAttribute("class");
-
